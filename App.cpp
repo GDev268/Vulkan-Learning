41785854d1e519c0ed60639055b9ff1219502040
GDev28 <gcgranja@gmail.com>
2023-05-03 22:46:19 +0100

    Keyboard Inputs

--- App.cpp
+++ App.cpp
@@ -2,6 +2,7 @@
 
 #include "RenderSystem.hpp"
 #include "Camera.hpp"
+#include "Keyboard.hpp"
 
 
 // libs
@@ -11,6 +12,8 @@
 #include <glm/gtc/constants.hpp>
 
 // std
+#include <iostream>
+#include <chrono>
 #include <array>
 #include <cassert>
 #include <stdexcept>
@@ -24,14 +27,27 @@ FirstApp::~FirstApp() {}
 void FirstApp::run() {
   RenderSystem renderSystem{device, renderer.getSwapChainRenderPass()};
   Camera camera{};
+  GameObject viewerObject = GameObject::createGameObject();
+  //camera.setViewDirection(glm::vec3{0.0f},glm::vec3{0.5f,0.0f,1.0f}); 
+  //camera.setViewTarget(glm::vec3(-1.0f,-2.0f,2.0f),glm::vec3(0.0f,0.0f,2.5f)); 
   float time = 0.0f;
+
+  auto currentTime = std::chrono::high_resolution_clock::now();
+  Keyboard cameraController{};
 
   while (!window.shouldClose()) {
     glfwPollEvents();
     
+    auto newTime = std::chrono::high_resolution_clock::now();
+    float deltaTime = std::chrono::duration<float,std::chrono::seconds::period>(newTime - currentTime).count();
+    currentTime = newTime;
+
+    cameraController.moveInPlaneXZ(window.getGLFWwindow(),deltaTime,viewerObject);
+    camera.setViewYXZ(viewerObject.transform.translation,viewerObject.transform.rotation);
+
     float aspect = renderer.getAspectRatio();
     //camera.setOrthographicProjection(-aspect,aspect,-1,1,-1,1);
-    camera.setPerspectiveProjection(glm::radians(90.0f),aspect,0.1f,100.0f);
+    camera.setPerspectiveProjection(glm::radians(50.0f),aspect,0.1f,100.0f);
 
     if (auto commandBuffer = renderer.beginFrame()) {
       renderer.beginSwapChainRenderPass(commandBuffer);
@@ -46,7 +62,7 @@ void FirstApp::run() {
   }
 
   vkDeviceWaitIdle(device.device());
-}
+} 
 
 // temporary helper function, creates a 1x1x1 cube centered at offset
 std::unique_ptr<Model> createCubeModel(Device& device, glm::vec3 offset) {
@@ -108,10 +124,10 @@ std::unique_ptr<Model> createCubeModel(Device& device, glm::vec3 offset) {
 }
 
 void FirstApp::loadGameObjects() {
-  std::shared_ptr<Model> lveModel = createCubeModel(device, {.0f, .0f, .0f});
+  std::shared_ptr<Model> model = createCubeModel(device, {.0f, .0f, .0f});
   auto cube = GameObject::createGameObject();
-  cube.model = lveModel;
-  cube.transform.translation = {.0f, .0f, 1.5f};
+  cube.model = model;
+  cube.transform.translation = {.0f, .0f, 2.5f};
   cube.transform.scale = {.5f, .5f, .5f};
   gameObjects.push_back(std::move(cube));
 }
--- RenderSystem.cpp
+++ RenderSystem.cpp
@@ -72,6 +72,8 @@ namespace Tutorial
   {
     lvePipeline->bind(commandBuffer);
 
+    auto projectionView = camera.getProjection() * camera.getView();
+
     for (auto &obj : gameObjects)
     {
       obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
@@ -79,7 +81,7 @@ namespace Tutorial
 
       SimplePushConstantData push{};
       push.color = obj.color;
-      push.transform = camera.getProjection() * obj.transform.mat4();
+      push.transform = projectionView * obj.transform.mat4();
 
       vkCmdPushConstants(
           commandBuffer,
